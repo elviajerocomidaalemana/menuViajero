2915b2649d9d0e04b8c75e36cd7c8fad79080e44
Sergio Hortua <sergioandresjsv@gmail.com>
2022-07-02 19:57:11 -0500

    modules test, salchichas and hamburger

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,106 @@ const Header = (props: Props) => {
           <img className="logo" src={logo} alt="" />
         </header>
       </Grid>
-      <Grid sx={{ }} item className="container__bottom">
+      <Grid sx={{ border: '1px solid red' }} item className="container__bottom">
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+          sx={{  }}
+          variant="text"
+        >
+          Cervezas
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <Link className="link" to="/">
+            <MenuItem onClick={handleClose}>Alemanas</MenuItem>
+          </Link>
+          <Link className="link" to="/holanda">
+            <MenuItem onClick={handleClose}>Holandesas</MenuItem>
+          </Link>
+          <Link className="link" to="/rusia">
+            <MenuItem onClick={handleClose}>Rusas</MenuItem>
+          </Link>
+          <Link className="link" to="/escocia">
+            <MenuItem onClick={handleClose}>Escocia</MenuItem>
+          </Link>
+          <Link className="link" to="/belgica">
+            <MenuItem onClick={handleClose}>Belgica</MenuItem>
+          </Link>
+          <Link className="link" to="/inglaterra">
+            <MenuItem onClick={handleClose}>Inglesas</MenuItem>
+          </Link>
+          <Link className="link" to="/australia">
+            <MenuItem onClick={handleClose}>Australianas</MenuItem>
+          </Link>
+          <Link className="link" to="/japon">
+            <MenuItem onClick={handleClose}>Japonesas</MenuItem>
+          </Link>
+          <Link className="link" to="/republica_checa">
+            <MenuItem onClick={handleClose}>Republica Checa</MenuItem>
+          </Link>
+        </Menu>
+      </Grid>
+      <Grid sx={{ border: '1px solid red' }} item className="container__bottom">
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+          sx={{  }}
+          variant="text"
+        >
+          Cervezas
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <Link className="link" to="/">
+            <MenuItem onClick={handleClose}>Alemanas</MenuItem>
+          </Link>
+          <Link className="link" to="/holanda">
+            <MenuItem onClick={handleClose}>Holandesas</MenuItem>
+          </Link>
+          <Link className="link" to="/rusia">
+            <MenuItem onClick={handleClose}>Rusas</MenuItem>
+          </Link>
+          <Link className="link" to="/escocia">
+            <MenuItem onClick={handleClose}>Escocia</MenuItem>
+          </Link>
+          <Link className="link" to="/belgica">
+            <MenuItem onClick={handleClose}>Belgica</MenuItem>
+          </Link>
+          <Link className="link" to="/inglaterra">
+            <MenuItem onClick={handleClose}>Inglesas</MenuItem>
+          </Link>
+          <Link className="link" to="/australia">
+            <MenuItem onClick={handleClose}>Australianas</MenuItem>
+          </Link>
+          <Link className="link" to="/japon">
+            <MenuItem onClick={handleClose}>Japonesas</MenuItem>
+          </Link>
+          <Link className="link" to="/republica_checa">
+            <MenuItem onClick={handleClose}>Republica Checa</MenuItem>
+          </Link>
+        </Menu>
+      </Grid><Grid sx={{ border: '1px solid red' }} item className="container__bottom">
         <Button
           id="basic-button"
           aria-controls={open ? "basic-menu" : undefined}
